e6c2da0004149e166b16337cfe59870f05de7729
Tom Wyllie <wyllietom@gmail.com>
2020-08-07 18:30:59 +0100

    Allow reusing pre-calculated CNN output in case of failure midway through RNN

--- scripts/evaluate-dataset/evaluate.py
+++ scripts/evaluate-dataset/evaluate.py
@@ -11,6 +11,7 @@ import numpy as np
 from folkfriend.cnn.denoiser import CNNDenoiser
 from folkfriend.data.data_ops import spec_to_pseudo
 from folkfriend.rnn.decoder import RNNDecoder
+from folkfriend import ff_config
 
 
 def main(cnn, rnn, dataset):
@@ -31,7 +32,6 @@ def main(cnn, rnn, dataset):
         dataset_sub_dir_path = os.path.join(out_dir, dataset_sub_dir_name)
         pathlib.Path(dataset_sub_dir_path).mkdir(parents=True, exist_ok=False)
 
-    from folkfriend import ff_config
     audio_samples_per_query = (ff_config.SAMPLE_RATE *
                                ff_config.AUDIO_QUERY_SECS)
     for dataset_entry in tqdm(recordings_data,
@@ -39,6 +39,19 @@ def main(cnn, rnn, dataset):
         audio_path = os.path.join(dataset, dataset_entry['path'])
         signal, sample_rate = audio2numpy.open_audio(audio_path)
         for i in range(signal.size // audio_samples_per_query):
+            base_img_path = os.path.join(out_dir, dataset_entry['path'])
+            base_img_path = base_img_path[:-4] + '-{}-{:d}.png'
+
+            stage_labels = [
+                base_img_path.format('a-spec', i),
+                base_img_path.format('b-mask', i),
+                base_img_path.format('c-denoised', i)
+            ]
+
+            if os.path.exists(stage_labels[-1]):
+                print(f'Found pre-existing path {stage_labels[-1]}')
+                continue
+
             original_spec = denoiser.load_spectrogram_from_signal_data(
                 sample_rate, signal[i * audio_samples_per_query:
                                     (i + 1) * audio_samples_per_query]
@@ -46,16 +59,10 @@ def main(cnn, rnn, dataset):
 
             mask, denoised = denoiser.denoise(original_spec)
 
-            base_img_path = os.path.join(dataset, dataset_entry['path'])
-            base_img_path = base_img_path[:-4] + '-{}-{:d}.png'
-
-            for path, img in (
-                    (base_img_path.format('a-spec', i), original_spec),
-                    (base_img_path.format('b-mask', i), mask),
-                    (base_img_path.format('c-denoised', i), denoised),
-            ):
-                imageio.imwrite(path, np.asarray(255 * img.T / np.max(img),
-                                                 dtype=np.uint8))
+            for path, img in zip(stage_labels,
+                                 (original_spec, mask, denoised)):
+                imageio.imwrite(path, np.asarray(
+                    255 * img.T / np.max(img), dtype=np.uint8))
 
     return
 
