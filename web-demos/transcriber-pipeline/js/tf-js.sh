fc98f4cc9e7fbf2222347eb2b23f5d6c0642a13a
Tom Wyllie <wyllietom@gmail.com>
2020-08-20 02:11:13 +0100

    Add WASM dependency

--- web-demos/transcriber-pipeline/js/tf-js.sh
+++ web-demos/transcriber-pipeline/js/tf-js.sh
@@ -1,1 +1,4 @@
+rm tf.min.js
+rm tf-backend-wasm.js
 wget https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.0.1/dist/tf.min.js
+wget https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm/dist/tf-backend-wasm.js
