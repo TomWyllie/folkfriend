c49cb92f89d33dc57c09827392aeacc1ff7e3f61
Tom Wyllie <wyllietom@gmail.com>
2020-08-30 20:38:36 +0100

    quick try catch workaround to allow processing to run to completion

--- web-demo/dataset-demo.js
+++ web-demo/dataset-demo.js
@@ -49,72 +49,77 @@ async function datasetDemo() {
 
     let t0 = Date.now();
     for(let i = 0; i < slices.data.length; i++) {
+        try {
 
-        // We already used python to convert the .wav slices to .pngs.
-        //  This meant we didn't need to mess around with WebAudio in NodeJS.
-        //  The results are almost pixel to pixel identical with only some
-        //  slight differences due to numerical precision / minutely different
-        //  windowing etc.
+            // We already used python to convert the .wav slices to .pngs.
+            //  This meant we didn't need to mess around with WebAudio in NodeJS.
+            //  The results are almost pixel to pixel identical with only some
+            //  slight differences due to numerical precision / minutely different
+            //  windowing etc.
 
-        let sliceURLWav = `${rootURL}/dataset/${slices.data[i].path}`;
-        let sliceURLPng = sliceURLWav.replace(".wav", ".png");
+            let sliceURLWav = `${rootURL}/dataset/${slices.data[i].path}`;
+            let sliceURLPng = sliceURLWav.replace(".wav", ".png");
 
-        // Now we have to decode the PNG ourselves and insert the data from each frame
-        //  into transcriber.featureExtractor.framesQueue
+            // Now we have to decode the PNG ourselves and insert the data from each frame
+            //  into transcriber.featureExtractor.framesQueue
 
-        const img = await Jimp.read(sliceURLPng);
+            const img = await Jimp.read(sliceURLPng);
 
-        transcriber.featureExtractor.flush();
+            transcriber.featureExtractor.flush();
 
-        for(let j = 0; j < img.bitmap.width; j++) {
-            // Float32 make tensorflow's tensors float32
-            let frame = new Float32Array(img.bitmap.height);
-            for(let k = 0; k < img.bitmap.height; k++) {
-                frame[k] = Jimp.intToRGBA(img.getPixelColor(j, k)).r
+            for(let j = 0; j < img.bitmap.width; j++) {
+                // Float32 make tensorflow's tensors float32
+                let frame = new Float32Array(img.bitmap.height);
+                for(let k = 0; k < img.bitmap.height; k++) {
+                    frame[k] = Jimp.intToRGBA(img.getPixelColor(j, k)).r
+                }
+                transcriber.featureExtractor.framesQueue.push(frame);
             }
-            transcriber.featureExtractor.framesQueue.push(frame);
-        }
 
-        // console.time("transcribe");
-        transcriber.featureExtractor.closed = true;
-        await transcriber.featureExtractor.bulkProceed();
-        const decoded = await transcriber.decode();
-        // console.timeEnd("transcribe");
+            // console.time("transcribe");
+            transcriber.featureExtractor.closed = true;
+            await transcriber.featureExtractor.bulkProceed();
+            const decoded = await transcriber.decode();
+            // console.timeEnd("transcribe");
 
-        // console.time("query");
-        let results = await queryEngine.query(decoded.decoded).catch(console.error);
-        // console.timeEnd("query");
+            // console.time("query");
+            let results = await queryEngine.query(decoded.decoded).catch(console.error);
+            // console.timeEnd("query");
 
-        // Now go through the results. Remember the ID of each result
-        //  is the *setting ID* that has been matched not the *tune ID*.
-        //  Record the highest score setting for each tune.
-        let tunesRanking = {};
-        let tunes = 0;
-        for(let i = 0; i < results.length; i++) {
-            let tune = settingsToTunes[parseInt(results[i].key)];
-            if(!tunesRanking.hasOwnProperty(tune)) {
-                tunesRanking[tune] = tunes;
-                tunes++;
+            // Now go through the results. Remember the ID of each result
+            //  is the *setting ID* that has been matched not the *tune ID*.
+            //  Record the highest score setting for each tune.
+            let tunesRanking = {};
+            let tunes = 0;
+            for(let i = 0; i < results.length; i++) {
+                let tune = settingsToTunes[parseInt(results[i].key)];
+                if(!tunesRanking.hasOwnProperty(tune)) {
+                    tunesRanking[tune] = tunes;
+                    tunes++;
+                }
             }
+
+            // Now let's look at the label for what the tune ID should have been
+            let groundTruth = slicesToTunes[slices.data[i].path];
+
+            // 0 means correct label was ranked first (best score).
+            //  Worst score is results.length (100) which means that
+            //  the tune was ranked outside the top results.length.
+            let score = tunesRanking[groundTruth] || results.length;
+
+            let dataLine = `${slices.data[i].path},${score}`;
+            console.info(dataLine);
+
+            let perf = (Date.now() - t0) / (1 + i);
+            perf = Math.round(1000 * perf) / 1000;
+            let minutesRemaining = perf * (slices.data.length - i - 1) / 60000;
+            minutesRemaining = Math.round(100 * minutesRemaining) / 100;
+            let updateInfo = `Progress: ${i} / ${slices.data.length}, at ${perf}ms per tune, (${minutesRemaining} minutes remaining)`;
+            console.info(updateInfo);
+
+        } catch (e) {
+            console.log(e);
         }
-
-        // Now let's look at the label for what the tune ID should have been
-        let groundTruth = slicesToTunes[slices.data[i].path];
-
-        // 0 means correct label was ranked first (best score).
-        //  Worst score is results.length (100) which means that
-        //  the tune was ranked outside the top results.length.
-        let score = tunesRanking[groundTruth] || results.length;
-
-        let dataLine = `${slices.data[i].path},${score}`;
-        console.info(dataLine);
-
-        let perf = (Date.now() - t0) / (1 + i);
-        perf = Math.round(1000 * perf) / 1000;
-        let minutesRemaining = perf * (slices.data.length - i - 1) / 60000;
-        minutesRemaining = Math.round(100 * minutesRemaining) / 100;
-        let updateInfo = `Progress: ${i} / ${slices.data.length}, at ${perf}ms per tune, (${minutesRemaining} minutes remaining)`;
-        console.info(updateInfo)
     }
 }
 
