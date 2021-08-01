f3497ad7726001212391d8942367b90c61fec671
Tom Wyllie <tom@wyllie.dev>
2021-08-01 22:52:52 +0100

    Parallelised query search in rust, also reverted poor parameter change

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,58 +1,59 @@
-mod folkfriend;
 mod dataset;
+mod folkfriend;
 
 use crate::folkfriend::index::structs::*;
+use clap::{App, Arg};
 use indicatif::ProgressBar;
+use rayon::prelude::*;
 use std::convert::TryInto;
 use std::time::Instant;
-use clap::{Arg, App};    
-
 
 fn main() {
     let now = Instant::now();
 
     let args = App::new("FolkFriend")
-    .version("3.0")
-    .author("T.C. Wyllie <tom@wyllie.dev>")
-    .about("Transcription and recognition of traditional instrumental folk music")
-    .subcommand(App::new("dataset")
-            .arg(Arg::new("dataset-op")
-            .possible_values(&["transcribe", "query"])
-            .required(true)
-            )
-            .arg(Arg::new("dataset-path")
-                .required(true)
-                .default_value("/home/tom/datasets/tiny-folkfriend-evaluation-dataset/")
-            )
+        .version("3.0")
+        .author("T.C. Wyllie <tom@wyllie.dev>")
+        .about("Transcription and recognition of traditional instrumental folk music")
+        .subcommand(
+            App::new("dataset")
+                .arg(
+                    Arg::new("dataset-op")
+                        .possible_values(&["transcribe", "query"])
+                        .required(true),
+                )
+                .arg(
+                    Arg::new("dataset-path")
+                        .required(true)
+                        .default_value("/home/tom/datasets/tiny-folkfriend-evaluation-dataset/"),
+                ),
         )
         .get_matches();
-        
-        if let Some(ref args) = args.subcommand_matches("dataset") {
-            
-            let dataset_path = args.value_of("dataset-path").unwrap().to_string();
-            let dataset_op = args.value_of("dataset-op").unwrap();
-            
-            match dataset_op {
-            "transcribe" => { println!("transcribing") },
-            "query" => { bulk_query(&dataset_path) },
-            _ => {},
+
+    if let Some(ref args) = args.subcommand_matches("dataset") {
+        let dataset_path = args.value_of("dataset-path").unwrap().to_string();
+        let dataset_op = args.value_of("dataset-op").unwrap();
+
+        match dataset_op {
+            "transcribe" => {
+                println!("transcribing")
+            }
+            "query" => bulk_query(&dataset_path),
+            _ => {}
         }
     }
-    
     println!("FolkFriend finished in {:.2?}", now.elapsed());
 }
 
 fn bulk_query(dataset_path: &String) {
-    let transcriptions = dataset::load_transcriptions(&dataset_path).unwrap(); 
+    let transcriptions = dataset::load_transcriptions(&dataset_path).unwrap();
     let tune_index = folkfriend::index::load::load_from_path();
     let tune_settings: TuneSettings = tune_index.settings;
     let query_engine = folkfriend::query::QueryEngine::new(tune_settings);
     
-    let mut bulk_output: Vec<dataset::TranscriptionRecordRanked> = Vec::new();
-
     let bar = ProgressBar::new(transcriptions.len().try_into().unwrap());
 
-    for transcription in transcriptions {
+    let ranks: Vec<usize> = transcriptions.par_iter().map(|transcription| {
         let query = &transcription.transcription;
         let ranked_settings = query_engine.run_query(query);
         let mut rank: usize = ranked_settings.len();
@@ -64,14 +65,21 @@ fn bulk_query(dataset_path: &String) {
             }
         }
 
-        bulk_output.push(dataset::TranscriptionRecordRanked {
-            rank: rank,
+        bar.inc(1);
+
+        rank
+    }).collect();
+    
+    let mut bulk_output: Vec<dataset::TranscriptionRecordRanked> = Vec::new();
+    for (rank, transcription) in ranks.iter().zip(transcriptions) {
+        let ranked = dataset::TranscriptionRecordRanked {
+            rank: *rank,
             rel_path: transcription.rel_path,
             tune_id: transcription.tune_id,
             name: transcription.name,
             transcription: transcription.transcription,
-        });
-        bar.inc(1);
+        };
+        bulk_output.push(ranked);
     }
 
     dataset::write_transcriptions_ranked(dataset_path, bulk_output).unwrap();
