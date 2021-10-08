b322a4eb1e0cec2b8032937ae10d6f9c7b72d18f
Tom Wyllie <tom@wyllie.dev>
2021-10-08 20:37:06 +0100

    Added Vue 2 app with Vuetify

---
+++ app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-app>
+        <v-app-bar app color="primary" dark>
+            <v-spacer></v-spacer>
+
+            <v-btn
+                href="https://github.com/vuetifyjs/vuetify/releases/latest"
+                target="_blank"
+                text
+            >
+                <span class="mr-2">Latest Release</span>
+                <v-icon>mdi-open-in-new</v-icon>
+            </v-btn>
+        </v-app-bar>
+
+        <v-main>
+            <router-view />
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+export default {
+    name: "App",
+
+    data: () => ({
+        //
+    }),
+};
+</script>
